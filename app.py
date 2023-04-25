8b0275701634d774efad61a080d285206eb2999f
Arj404 <64511698+awesomearjun@users.noreply.github.com>
2023-04-25 01:32:36 +0000

    Made the user input go in database

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
+connection = sqlite3.connect("blogs.db", check_same_thread=False)
+cursor = connection.cursor()
 
 
 @app.route("/")
@@ -10,4 +13,24 @@ def index():
 
 @app.route("/register", methods=["POST"])
 def register():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    print(username, password)
+
+    if not username or not password:
+        return
+    cursor.execute(
+        "INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+    connection.commit()
+
+    return render_template("success.html")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    
+     
     return render_template("success.html")
